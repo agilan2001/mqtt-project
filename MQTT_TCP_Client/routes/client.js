3926e5aad1d15c1f8ee4a6b66353d1e24fd51bbd
aiyanarappan <Agilan.Iyanarappan@viasat.com>
2021-06-22 18:46:57 +0530

    init

---
+++ MQTT_TCP_Client/routes/client.js
@@ -0,0 +1,55 @@
+var express = require('express');
+
+var router = express.Router();
+
+var mqtt = require('mqtt')
+
+var clients = {};
+
+router.post('/connect', function(req, res, next) {
+    var clientId = req.body.clientId
+    var client = mqtt.connect("mqtt://localhost", {clientId:clientId})
+    client.on('connect',function(){
+        clients[clientId] = {}
+        clients[clientId].client = client
+        res.send("client connected")
+    })
+});
+
+router.post('/subscribe', function(req, res, next) {
+    var {clientId,topic,qos} = req.body;
+    var client = clients[clientId].client
+    client.subscribe(topic,{qos:qos}, function(err){
+        if(!err){
+            res.send(`Subscribed ${clientId} to ${topic} with QOS ${qos}`)
+        }
+    })
+});
+
+router.post('/publish', function(req, res, next) {
+    var {clientId,topic,qos, message, retain} = req.body;
+    var client = clients[clientId].client
+    client.publish(topic, message,{qos:qos, retain: retain}, function(err){
+        if(!err){
+            res.send(`Published to ${topic} with QOS ${qos}`)
+        }
+    })
+});
+
+
+
+var appWs = express()
+var expressWs = require('express-ws')(appWs);
+
+appWs.ws("/client_ws", function(ws,req){
+  ws.on("message",function(mes){
+      clients[mes].socket = ws
+      clients[mes].client.on("message", function(topic, message){
+          ws.send(JSON.stringify({topic:topic, message: message}))
+      })
+
+  })
+})
+appWs.listen(5000)
+
+module.exports = router;
