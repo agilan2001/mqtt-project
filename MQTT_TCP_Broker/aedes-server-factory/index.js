e9681b934e3c81cf2e8de88cd232aca999859045
aiyanarappan <Agilan.Iyanarappan@viasat.com>
2021-07-02 13:45:13 +0530

    quic idle timeout

--- MQTT_TCP_Broker/aedes-server-factory/index.js
+++ MQTT_TCP_Broker/aedes-server-factory/index.js
@@ -2,14 +2,17 @@ const fs = require('fs')
 const aedes = require('aedes')();
 const serverFactory = require('aedes-server-factory');
 
-// const server = require('aedes-server-factory').createServer(aedes, 
-//     { 
-//         // quic: {
-//         //     key: fs.readFileSync('./key.pem'),
-//         //     cert: fs.readFileSync('./cert.pem'),
-//         //     //port: 8885
-//         // }
-//     });
+const quic_server = require('aedes-server-factory').createServer(aedes,
+    {
+        quic: {
+            key: fs.readFileSync('./key.pem'),
+            cert: fs.readFileSync('./cert.pem'),
+            //port: 8885
+        }
+    });
+quic_server.listen({
+    idleTimeout: 0 //disable timeout
+});
 
 const tls_server = serverFactory.createServer(aedes, {
     tls: {
