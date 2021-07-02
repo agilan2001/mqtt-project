a41e1f1a1fa78be5d0d83d5f08dfbd36b3fe398e
aiyanarappan <Agilan.Iyanarappan@viasat.com>
2021-07-02 23:10:16 +0530

    tls_log for server

--- MQTT_TCP_Broker/aedes-server-factory/index.js
+++ MQTT_TCP_Broker/aedes-server-factory/index.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const aedes = require('aedes')();
 const serverFactory = require('aedes-server-factory');
 
+const log = fs.createWriteStream('tls_log.txt', { flags: 'a' });
+
 const quic_server = require('aedes-server-factory').createServer(aedes,
     {
         quic: {
@@ -21,6 +23,11 @@ const tls_server = serverFactory.createServer(aedes, {
         rejectUnauthorized: false,
         minVersion: 'TLSv1.3'
     }
+})
+
+tls_server.on('keylog', (line) => log.write(line))
+quic_server.on('session',(req)=>{
+    req.on('keylog',(line)=>log.write(line))
 })
 
 tls_server.listen(1885)
