cc50020c4872f4d0c055ddbfa278df3ff6e22660
aiyanarappan <Agilan.Iyanarappan@viasat.com>
2021-07-07 20:48:10 +0530

    counter based recursive termination

--- MQTT_Client/api/lab.js
+++ MQTT_Client/api/lab.js
@@ -30,6 +30,7 @@ function bomb_test(ws, val) {
     var TCP_rec_cnt = 0;
     var QUIC_sent_cnt = 0;
     var QUIC_rec_cnt = 0;
+    var inter_cnt = 0
 
     //************ Clinet TCP SEND
     var client_TCP_send = mqtt.connect(`mqtts://${val.broker}:1885`,
@@ -98,26 +99,32 @@ function bomb_test(ws, val) {
     var bomb_inter = setInterval(() => {
         ws.send(JSON.stringify({ TCP_sent_cnt, QUIC_sent_cnt, TCP_rec_cnt, QUIC_rec_cnt }))
         TCP_sent_cnt = 0; QUIC_sent_cnt = 0; TCP_rec_cnt = 0; QUIC_rec_cnt = 0;
+
+        inter_cnt++;
+        if (inter_cnt == val.inter_cnt) {
+            console.log('bomb_end');
+            client_TCP_send.end(); client_TCP_rec.end(); client_QUIC_send.end(); client_QUIC_rec.end();
+            clearInterval(bomb_inter);
+            ws.close();
+        }
+
+
     }, 1000 * val.upd_inter)
-
-    ws.on('close', () => {
-        console.log('bomb_end');
-        client_TCP_send.end(); client_QUIC_send.end(); client_TCP_rec.end(); client_QUIC_rec.end();
-        clearInterval(bomb_inter);
-    })
-
 
 }
 
-var TCP_pub_time;
-var TCP_rec_time;
-var QUIC_pub_time;
-var QUIC_rec_time;
-
 function round_trip(ws, val) {
 
+    var TCP_pub_time;
+    var TCP_rec_time;
+    var QUIC_pub_time;
+    var QUIC_rec_time;
+
+    var TCP_trial_cnt = 0;
+    var QUIC_trial_cnt = 0;
+
+
     //************ Client TCP 
-    var tcp_rtt_timeout;
     var TCP_client = mqtt.connect(`mqtts://${val.broker}:1885`,
         { rejectUnauthorized: false, keepalive: 60 })
 
@@ -128,12 +135,22 @@ function round_trip(ws, val) {
                 TCP_rec_time = Date.now();
                 ws.send(JSON.stringify({ TCP_RTT: TCP_rec_time - TCP_pub_time }))
 
-                tcp_rtt_timeout = setTimeout(() => {
-                    TCP_client.publish('rtt_test', 'rtt', () => {
-                        TCP_pub_time = Date.now();
-                    })
-                }, 2000)
+                TCP_trial_cnt++;
+                if (TCP_trial_cnt < val.trial_cnt) {
 
+                    setTimeout(() => {
+                        TCP_client.publish('rtt_test', 'rtt', () => {
+                            TCP_pub_time = Date.now();
+                        })
+                    }, 2000)
+
+                } else {
+                    TCP_client.end();
+                    console.log('rtt_tcp_end');
+                    if (QUIC_trial_cnt == val.trial_cnt) {
+                        ws.close()
+                    }
+                }
             })
 
             TCP_client.publish('rtt_test', 'rtt', () => {
@@ -143,7 +160,6 @@ function round_trip(ws, val) {
     })
 
     //************ Client QUIC 
-    var quic_rtt_timeout;
     var QUIC_client = mqtt.connect(`quic://${val.broker}:8885`,
         { rejectUnauthorized: false, keepalive: 60 })
 
@@ -154,12 +170,20 @@ function round_trip(ws, val) {
                 QUIC_rec_time = Date.now();
                 ws.send(JSON.stringify({ QUIC_RTT: QUIC_rec_time - QUIC_pub_time }))
 
-                quic_rtt_timeout = setTimeout(() => {
-                    QUIC_client.publish('rtt_quic', 'rtt', () => {
-                        QUIC_pub_time = Date.now();
-                    })
-                }, 2000)
-
+                QUIC_trial_cnt++;
+                if (QUIC_trial_cnt < val.trial_cnt) {
+                    setTimeout(() => {
+                        QUIC_client.publish('rtt_quic', 'rtt', () => {
+                            QUIC_pub_time = Date.now();
+                        })
+                    }, 2000)
+                } else {
+                    QUIC_client.end();
+                    console.log('rtt_quic_end');
+                    if (TCP_trial_cnt == val.trial_cnt) {
+                        ws.close()
+                    }
+                }
             })
 
             QUIC_client.publish('rtt_quic', 'rtt', () => {
@@ -168,25 +192,27 @@ function round_trip(ws, val) {
         })
     })
 
-    ws.on('close', () => {
-        console.log('rtt_end');
-        QUIC_client.end(); TCP_client.end()
-        clearTimeout(tcp_rtt_timeout);
-        clearTimeout(quic_rtt_timeout);
-    })
+    // ws.on('close', () => {
+    //     console.log('rtt_end');
+    //     QUIC_client.end(); TCP_client.end()
+    //     clearTimeout(tcp_rtt_timeout);
+    //     clearTimeout(quic_rtt_timeout);
+    // })
 
 }
 
 
-var TCP_cnct_time;
-var TCP_cnak_time;
-var QUIC_cnct_time;
-var QUIC_cnak_time;
-
 function reincarnate_time(ws, val) {
 
+    var TCP_cnct_time;
+    var TCP_cnak_time;
+    var QUIC_cnct_time;
+    var QUIC_cnak_time;
+
+    var TCP_trial_cnt = 0;
+    var QUIC_trial_cnt = 0;
+
     //************ Client TCP 
-    var tcp_rit_timeout;
     TCP_cnct_time = Date.now();
     var TCP_client = mqtt.connect(`mqtts://${val.broker}:1885`,
         { rejectUnauthorized: false, keepalive: 60 })
@@ -197,14 +223,21 @@ function reincarnate_time(ws, val) {
         ws.send(JSON.stringify({ TCP_RIT: TCP_cnak_time - TCP_cnct_time }))
         TCP_client.end();
 
-        tcp_rit_timeout = setTimeout(() => {
-            TCP_cnct_time = Date.now();
-            TCP_client.reconnect()
-        }, 2000)
+        TCP_trial_cnt++;
+        if (TCP_trial_cnt < val.trial_cnt) {
+            setTimeout(() => {
+                TCP_cnct_time = Date.now();
+                TCP_client.reconnect()
+            }, 2000)
+        } else {
+            console.log('TCP_rit_end')
+            if (QUIC_trial_cnt == val.trial_cnt) {
+                ws.close()
+            }
+        }
     })
 
     //************ Client QUIC 
-    var quic_rit_timeout;
     QUIC_cnct_time = Date.now();
     var QUIC_client = mqtt.connect(`quic://${val.broker}:8885`,
         { rejectUnauthorized: false, keepalive: 60 })
@@ -215,18 +248,26 @@ function reincarnate_time(ws, val) {
         ws.send(JSON.stringify({ QUIC_RIT: QUIC_cnak_time - QUIC_cnct_time }))
         QUIC_client.end();
 
-        quic_rit_timeout = setTimeout(() => {
-            QUIC_cnct_time = Date.now();
-            QUIC_client.reconnect()
-        }, 2000)
+        QUIC_trial_cnt++;
+        if (QUIC_trial_cnt < val.trial_cnt) {
+            setTimeout(() => {
+                QUIC_cnct_time = Date.now();
+                QUIC_client.reconnect()
+            }, 2000)
+        } else {
+            console.log('QUIC_rit_end');
+            if (TCP_trial_cnt == val.trial_cnt) {
+                ws.close();
+            }
+        }
     })
 
-    ws.on('close', () => {
-        console.log('rit_end');
-        console.log(tcp_rit_timeout)
-        clearTimeout(tcp_rit_timeout);
-        clearTimeout(quic_rit_timeout);
-        QUIC_client.end(); TCP_client.end();
-    })
+    // ws.on('close', () => {
+    //     console.log('rit_end');
+    //     console.log(tcp_rit_timeout)
+    //     clearTimeout(tcp_rit_timeout);
+    //     clearTimeout(quic_rit_timeout);
+    //     QUIC_client.end(); TCP_client.end();
+    // })
 
 }
--- MQTT_Client/public/javascripts/lab_script.js
+++ MQTT_Client/public/javascripts/lab_script.js
@@ -201,103 +201,120 @@ var ws_bomb;
 
 function bomb_click() {
 
+    // if (btn_bomb.innerHTML == "BOMB") {
+    //     btn_bomb.innerHTML = "STOP";
+    //     btn_bomb.classList.remove('btn-primary');
+    //     btn_bomb.classList.add('btn-danger');
 
-    if (btn_bomb.innerHTML == "BOMB") {
-        btn_bomb.innerHTML = "STOP";
-        btn_bomb.classList.remove('btn-primary');
-        btn_bomb.classList.add('btn-danger');
+    btn_bomb.classList.add('disabled')
 
-        var cnt = 1;
-        bomb_send_chart.data.labels = [];
-        bomb_send_chart.data.datasets[0].data = [];
-        bomb_send_chart.data.datasets[1].data = [];
+    var cnt = 1;
+    bomb_send_chart.data.labels = [];
+    bomb_send_chart.data.datasets[0].data = [];
+    bomb_send_chart.data.datasets[1].data = [];
+    bomb_send_chart.update()
+
+    bomb_rec_chart.data.labels = [];
+    bomb_rec_chart.data.datasets[0].data = [];
+    bomb_rec_chart.data.datasets[1].data = [];
+    bomb_rec_chart.update()
+
+    ws_bomb = new WebSocket(`ws://${window.location.host.split(":")[0] + ":5500"}/lab_ws`);
+    ws_bomb.addEventListener("open", () => {
+        ws_bomb.send(JSON.stringify({
+            param: 'bomb',
+            broker: broker_url_txt.value,
+            TCP_port: parseInt(tcp_broker_port_txt.value),
+            QUIC_port: parseInt(quic_broker_port_txt.value),
+            upd_inter: parseInt(upd_inter_txt.value),
+            inter_cnt: parseInt(inter_cnt_txt.value)
+        }))
+    })
+
+    ws_bomb.addEventListener('message', (event) => {
+        var val = JSON.parse(event.data);
+
+        bomb_send_chart.data.labels.push(cnt);
+        bomb_send_chart.data.datasets[0].data.push(val.TCP_sent_cnt)
+        bomb_send_chart.data.datasets[1].data.push(val.QUIC_sent_cnt)
         bomb_send_chart.update()
 
-        bomb_rec_chart.data.labels = [];
-        bomb_rec_chart.data.datasets[0].data = [];
-        bomb_rec_chart.data.datasets[1].data = [];
+        bomb_rec_chart.data.labels.push(cnt);
+        bomb_rec_chart.data.datasets[0].data.push(val.TCP_rec_cnt)
+        bomb_rec_chart.data.datasets[1].data.push(val.QUIC_rec_cnt)
         bomb_rec_chart.update()
 
-        ws_bomb = new WebSocket(`ws://${window.location.host.split(":")[0] + ":5500"}/lab_ws`);
-        ws_bomb.addEventListener("open", () => {
-            ws_bomb.send(JSON.stringify({
-                param: 'bomb',
-                broker: broker_url_txt.value,
-                TCP_port: parseInt(tcp_broker_port_txt.value),
-                QUIC_port: parseInt(quic_broker_port_txt),
-                upd_inter: parseInt(upd_inter_txt.value)
-            }))
-        })
+        cnt++;
 
-        ws_bomb.addEventListener('message', (event) => {
-            var val = JSON.parse(event.data);
+    })
 
-            bomb_send_chart.data.labels.push(cnt);
-            bomb_send_chart.data.datasets[0].data.push(val.TCP_sent_cnt)
-            bomb_send_chart.data.datasets[1].data.push(val.QUIC_sent_cnt)
-            bomb_send_chart.update()
+    ws_bomb.addEventListener('close', () => {
+        console.log('bomb_done');
+        btn_bomb.classList.remove('disabled');
+    })
 
-            bomb_rec_chart.data.labels.push(cnt);
-            bomb_rec_chart.data.datasets[0].data.push(val.TCP_rec_cnt)
-            bomb_rec_chart.data.datasets[1].data.push(val.QUIC_rec_cnt)
-            bomb_rec_chart.update()
-
-            cnt++;
-
-        })
-    } else {
-        btn_bomb.innerHTML = "BOMB";
-        btn_bomb.classList.remove('btn-danger');
-        btn_bomb.classList.add('btn-primary');
-        ws_bomb.close()
-    }
+    // } else {
+    //     btn_bomb.innerHTML = "BOMB";
+    //     btn_bomb.classList.remove('btn-danger');
+    //     btn_bomb.classList.add('btn-primary');
+    //     ws_bomb.close()
+    // }
 }
 
 var ws_rtt;
 
 function rtt_click() {
 
-    if (btn_rtt.innerHTML == "START") {
-        btn_rtt.innerHTML = "STOP";
-        btn_rtt.classList.remove('btn-primary');
-        btn_rtt.classList.add('btn-danger');
+    // if (btn_rtt.innerHTML == "START") {
+    //     btn_rtt.innerHTML = "STOP";
+    //     btn_rtt.classList.remove('btn-primary');
+    //     btn_rtt.classList.add('btn-danger');
 
-        rtt_chart.data.labels = [];
-        rtt_chart.data.datasets[0].data = [];
-        rtt_chart.data.datasets[1].data = [];
+    btn_rtt.classList.add('disabled');
+
+    rtt_chart.data.labels = [];
+    rtt_chart.data.datasets[0].data = [];
+    rtt_chart.data.datasets[1].data = [];
+    rtt_chart.update()
+
+    ws_rtt = new WebSocket(`ws://${window.location.host.split(":")[0] + ":5500"}/lab_ws`);
+    ws_rtt.addEventListener("open", () => {
+        ws_rtt.send(JSON.stringify({
+            param: 'rtt',
+            broker: rtt_broker_url_txt.value,
+            TCP_port: parseInt(rtt_tcp_broker_port_txt.value),
+            QUIC_port: parseInt(rtt_quic_broker_port_txt.value),
+            trial_cnt: parseInt(rtt_trial_cnt.value)
+        }))
+    })
+
+    ws_rtt.addEventListener('message', (event) => {
+        var val = JSON.parse(event.data);
+
+        if ('TCP_RTT' in val) {
+            // console.log(val.TCP_RTT)
+            rtt_chart.data.datasets[0].data.push(val.TCP_RTT)
+        } else {
+            rtt_chart.data.datasets[1].data.push(val.QUIC_RTT)
+        }
+
+
+        rtt_chart.data.labels = [...Array(Math.max(rtt_chart.data.datasets[0].data.length, rtt_chart.data.datasets[1].data.length) + 1).keys()].slice(1)
+        // console.log(rtt_chart.data.labels)
         rtt_chart.update()
+    })
 
-        ws_rtt = new WebSocket(`ws://${window.location.host.split(":")[0] + ":5500"}/lab_ws`);
-        ws_rtt.addEventListener("open", () => {
-            ws_rtt.send(JSON.stringify({
-                param: 'rtt',
-                broker: rtt_broker_url_txt.value,
-                TCP_port: parseInt(rtt_tcp_broker_port_txt.value),
-                QUIC_port: parseInt(rtt_quic_broker_port_txt),
-            }))
-        })
-
-        ws_rtt.addEventListener('message', (event) => {
-            var val = JSON.parse(event.data);
-
-            if ('TCP_RTT' in val) {
-                // console.log(val.TCP_RTT)
-                rtt_chart.data.datasets[0].data.push(val.TCP_RTT)
-            } else {
-                rtt_chart.data.datasets[1].data.push(val.QUIC_RTT)
-            }
-
-        
-            rtt_chart.data.labels = [...Array(Math.max(rtt_chart.data.datasets[0].data.length, rtt_chart.data.datasets[1].data.length)+1).keys()].slice(1)
-            // console.log(rtt_chart.data.labels)
-            rtt_chart.update()
-        })
-    } else {
-        btn_rtt.innerHTML = "START";
-        btn_rtt.classList.remove('btn-danger');
-        btn_rtt.classList.add('btn-primary');
-        ws_rtt.close()
-    }
+    ws_rtt.addEventListener('close', () => {
+        console.log('rtt_done');
+        btn_rtt.classList.remove('disabled');
+    })
+    
+    // } else {
+    //     btn_rtt.innerHTML = "START";
+    //     btn_rtt.classList.remove('btn-danger');
+    //     btn_rtt.classList.add('btn-primary');
+    //     ws_rtt.close()
+    // }
 }
 
 
@@ -305,45 +322,52 @@ var ws_rit;
 
 function rit_click() {
 
-    if (btn_rit.innerHTML == "START") {
-        btn_rit.innerHTML = "STOP";
-        btn_rit.classList.remove('btn-primary');
-        btn_rit.classList.add('btn-danger');
+    // if (btn_rit.innerHTML == "START") {
+    //     btn_rit.innerHTML = "STOP";
+    //     btn_rit.classList.remove('btn-primary');
+    //     btn_rit.classList.add('btn-danger');
 
-        rit_chart.data.labels = [];
-        rit_chart.data.datasets[0].data = [];
-        rit_chart.data.datasets[1].data = [];
+    btn_rit.classList.add('disabled');
+
+    rit_chart.data.labels = [];
+    rit_chart.data.datasets[0].data = [];
+    rit_chart.data.datasets[1].data = [];
+    rit_chart.update()
+
+    ws_rit = new WebSocket(`ws://${window.location.host.split(":")[0] + ":5500"}/lab_ws`);
+    ws_rit.addEventListener("open", () => {
+        ws_rit.send(JSON.stringify({
+            param: 'rit',
+            broker: rit_broker_url_txt.value,
+            TCP_port: parseInt(rit_tcp_broker_port_txt.value),
+            QUIC_port: parseInt(rit_quic_broker_port_txt),
+            trial_cnt: parseInt(rit_trial_cnt.value)
+        }))
+    })
+
+    ws_rit.addEventListener('message', (event) => {
+        var val = JSON.parse(event.data);
+
+        if ('TCP_RIT' in val) {
+            rit_chart.data.datasets[0].data.push(val.TCP_RIT)
+        } else {
+            rit_chart.data.datasets[1].data.push(val.QUIC_RIT)
+        }
+
+
+        rit_chart.data.labels = [...Array(Math.max(rit_chart.data.datasets[0].data.length, rit_chart.data.datasets[1].data.length) + 1).keys()].slice(1)
+        // console.log(rtt_chart.data.labels)
         rit_chart.update()
+    })
 
-        ws_rit = new WebSocket(`ws://${window.location.host.split(":")[0] + ":5500"}/lab_ws`);
-        ws_rit.addEventListener("open", () => {
-            ws_rit.send(JSON.stringify({
-                param: 'rit',
-                broker: rit_broker_url_txt.value,
-                TCP_port: parseInt(rit_tcp_broker_port_txt.value),
-                QUIC_port: parseInt(rit_quic_broker_port_txt),
-            }))
-        })
-
-        ws_rit.addEventListener('message', (event) => {
-            var val = JSON.parse(event.data);
-
-            if ('TCP_RIT' in val) {
-                console.log(val.TCP_RIT)
-                rit_chart.data.datasets[0].data.push(val.TCP_RIT)
-            } else {
-                rit_chart.data.datasets[1].data.push(val.QUIC_RIT)
-            }
-
-        
-            rit_chart.data.labels = [...Array(Math.max(rit_chart.data.datasets[0].data.length, rit_chart.data.datasets[1].data.length)+1).keys()].slice(1)
-            // console.log(rtt_chart.data.labels)
-            rit_chart.update()
-        })
-    } else {
-        btn_rit.innerHTML = "START";
-        btn_rit.classList.remove('btn-danger');
-        btn_rit.classList.add('btn-primary');
-        ws_rit.close()
-    }
+    ws_rit.addEventListener('close',()=>{
+        console.log('rit_done');
+        btn_rit.classList.remove('disabled')
+    })
+    // } else {
+    //     btn_rit.innerHTML = "START";
+    //     btn_rit.classList.remove('btn-danger');
+    //     btn_rit.classList.add('btn-primary');
+    //     ws_rit.close()
+    // }
 }
